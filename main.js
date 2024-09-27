5e6437db7915519401bbe3f693efb98fa9025776
FORTUNER001 <77770386+fortuner001@users.noreply.github.com>
2024-09-27 13:45:23 +0500

    V0.32

---
+++ main.js
@@ -0,0 +1,10 @@
+let a = 5;
+let b = 14;
+let c = (a+b)*(b+a)
+
+console.log(c);
+
+document.getElementById('num').innerHTML=c;
+function btn31() {
+    alert("Правда, лучше посмотрите на википедии, нежели тут :3");
+  }
